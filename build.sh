57baee88bbbf353eab137102c779286f0ada0d06
Marc Anton Dahmen <mail@marcdahmen.de>
2026-09-10 19:30:09 +0200

    update design

--- build.sh
+++ build.sh
@@ -6,14 +6,15 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 gallery=""
 
 while IFS=$'\t' read -r title url screenshot; do
-	item=$(cat <<-ITEM
-		<a class="gallery-item" href="${url}" target="_blank" rel="noopener">
-			<div class="gallery-frame">
-				<img src="screenshots/${screenshot}" alt="${title}" loading="lazy">
-			</div>
-			<span class="gallery-title">${title}</span>
-		</a>
-	ITEM
+	item=$(
+		cat <<-ITEM
+			<a class="gallery-item" href="${url}" target="_blank" rel="noopener">
+				<div class="gallery-frame">
+					<img src="screenshots/${screenshot}" alt="${title}" loading="lazy">
+				</div>
+				<span class="gallery-title">${title} ⟶</span>
+			</a>
+		ITEM
 	)
 	gallery+="${item}"$'\n'
 done < <(jq -r '.[] | [.title, .url, .screenshot] | @tsv' sites.json)
@@ -21,4 +22,4 @@ done < <(jq -r '.[] | [.title, .url, .screenshot] | @tsv' sites.json)
 template=$(cat template.html)
 output="${template//__SITES__/$gallery}"
 
-printf '%s\n' "$output" > public/index.html
+printf '%s\n' "$output" >public/index.html
